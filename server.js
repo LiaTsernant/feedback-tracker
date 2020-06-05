dce867f5b5b90955f1ba5e7bacc245488b8ac6f1
Yulia Tsernant <yulia.tsernant@d-d.me>
2020-06-05 16:48:28 -0700

    Added a path for api routes

--- server.js
+++ server.js
@@ -7,26 +7,18 @@ const routes = require('./routes'); // Routes Module
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
 
+//Custom logger from Kenny to see in the console what routes I am trying to access
 app.use((req, res, next) => {
   const url = req.url;
   const method = req.method;
   const requestedAt = new Date().toLocaleTimeString();
   const result = `${method} ${url} ${requestedAt}`;
   console.log(result);
-
   next();
 });
 
 app.use('/', routes.views);
-
-
-
-
-
-
-
-
-
+app.use('/api/v1/protected_routes', routes.api);
 
 // -------------------------------------------------------------------------
 app.listen(PORT, () => console.log(`Project is listening at http:localhost:${PORT}`));
