04f3fd1dfe9874dd1acfd9fe75b4534c3bfa0ef6
Yulia Tsernant <yulia.tsernant@d-d.me>
2020-06-05 17:20:32 -0700

    Created GET /data to see how the doc from database looks like

--- routes/api.js
+++ routes/api.js
@@ -1,2 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const doc = require('../spreadshet')
+
+//api/v1/protected_routes
+
+router.get('/doc', (req, res) => {
+  res.sendStatus(200).json({ doc })
+});
+
+module.exports = router;
