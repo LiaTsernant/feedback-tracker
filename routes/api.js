4c51df5002f2fcc59c52c8a76a985eb6ff5182f3
Yulia Tsernant <yulia.tsernant@d-d.me>
2020-06-05 21:58:14 -0700

    Created a function that return all information about GoogleSheet

--- routes/api.js
+++ routes/api.js
@@ -4,20 +4,10 @@ const doc = require('../spreadshet')
 
 //api/v1/protected_routes
 
+//Get information about all sheets
 router.get('/doc', (req, res) => {
   doc.then((data => {
-    let sheetsCount = data.sheetsByIndex.length
-    let result = [];
-    for (let i = 0; i < sheetsCount; i += 1) {
-      let sheet = data.sheetsByIndex[i];
-      let sheetObj = {
-        title: sheet.title
-      }
-
-      result.push(sheetObj)
-    }
-
-    res.json(result);
+    res.json(data);
   }));
 });
 
--- spreadshet.js
+++ spreadshet.js
@@ -1,28 +1,51 @@
 const { GoogleSpreadsheet } = require('google-spreadsheet');
 
-// async function accessFirstSheet() {
-//   const doc = new GoogleSpreadsheet('1CvttHrbkh-LtZtu-Xfa8zJ1sM2y9EaiBpFpngT7FoYs');
-//   await doc.useServiceAccountAuth(require('./client_secret.json'));
-//   await doc.loadInfo(); // loads document properties and worksheets
-//   const sheet = doc.sheetsByIndex[0]; // or use doc.sheetsById[id]
-//   const rows = await sheet.getRows(); // can pass in { limit, offset }
-//   console.log(rows[1]['Full Name'])
-// }
-
 async function getDocWithSheets() {
   const doc = new GoogleSpreadsheet('1CvttHrbkh-LtZtu-Xfa8zJ1sM2y9EaiBpFpngT7FoYs');
   await doc.useServiceAccountAuth(require('./client_secret.json'));
   await doc.loadInfo(); // loads document properties and worksheets
-  return doc;
+  let sheetsCount = doc.sheetsByIndex.length;
+  let allSheetsData = [];
+
+  // Iteration over all giant sheets objects
+  for (let i = 0; i < sheetsCount; i += 1) {
+    let rawSheet = doc.sheetsByIndex[i];
+    //Creating only data we need
+    let sheetObj = {
+      title: doc.sheetsByIndex[i].title,
+      students: []
+    };
+
+    const rows = await rawSheet.getRows().
+      then((rowRes => {
+        for (let j = 0; j < rowRes.length; j += 1) {
+          let student = {
+            name: rowRes[j]['Full Name'],
+            photo: rowRes[j].Photo,
+            Week1: rowRes[j].Week1,
+            Week2: rowRes[j].Week2,
+            Week3: rowRes[j].Week3,
+            Week4: rowRes[j].Week4,
+            Week5: rowRes[j].Week5,
+            Week6: rowRes[j].Week6,
+            Week7: rowRes[j].Week7,
+            Week8: rowRes[j].Week8,
+            Week9: rowRes[j].Week9,
+            Week10: rowRes[j].Week10,
+            Week11: rowRes[j].Week11,
+            Week12: rowRes[j].Week12,
+          }
+
+          sheetObj.students.push(student);
+        };
+      }));
+    
+    allSheetsData.push(sheetObj);
+  };
+
+  return allSheetsData;
 };
 
 const data = getDocWithSheets();
-
-// const sheet = doc.sheetsByIndex[0]; // or use doc.sheetsById[id]
-// const rows = await sheet.getRows(); // can pass in { limit, offset }
-// console.log(rows)
-
-// accessFirstSheet()
-// getDocWithSheets()
 
 module.exports = data;
