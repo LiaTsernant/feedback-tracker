ce1c6ec787db4c7b3b814e564fc3e159c6214fa9
Yulia Tsernant <yulia.tsernant@d-d.me>
2020-06-06 17:02:43 -0700

    Added function to update database

--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,33 @@
 const express = require("express");
 const router = express.Router();
-const doc = require('../spreadshet')
+const getDataFromSpreadsheet = require('../getDataFromSpreadsheet');
+const db = require('../models')
 
 //api/v1/protected_routes
 
 //Get information about all sheets
-router.get('/doc', (req, res) => {
-  doc.then((data => {
-    res.json(data);
+router.get('/update_database', (req, res) => {
+  getDataFromSpreadsheet().then((data => {
+    // console.log(data)
+    db.Student.deleteMany({}, (err, result) => {
+      if (err) {
+        console.log(err);
+        process.exit();
+      };
+      console.log(`Successfully deleted ${result.deletedCount} students.`);
+
+      db.Student.create(data, (err, newStudents) => {
+        if (err) {
+          console.log(err);
+          // process.exit();
+        };
+        let retStudents = [];
+        for (let i = 0; i < newStudents.length; i += 1) {
+          retStudents.push(`${newStudents[i].studentId}`);
+        }
+        res.json(retStudents)
+      })
+    })
   }));
 });
 
