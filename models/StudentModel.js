58aa3670163c03b4330511e57f8d91f9079c45fc
Yulia Tsernant <yulia.tsernant@d-d.me>
2020-06-06 17:04:10 -0700

    Added StudentModel

---
+++ models/StudentModel.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const StudentSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  courseTitle: String,
+  studentId: Number,
+  photo: String,
+  Week1: String, 
+  Week2: String,
+  Week3: String,
+  Week4: String,
+  Week5: String,
+  Week6: String,
+  Week7: String,
+  Week8: String,
+  Week9: String,
+  Week10: String,
+  Week11: String,
+  Week12: String,
+}, { timestamps: true });
+
+const Post = mongoose.model('Student', StudentSchema);
+
+module.exports = Post;
