e0e4aa5cf622b5cd7b56b9ecb5f86b2d8c43e649
Yulia Tsernant <yulia.tsernant@d-d.me>
2020-07-07 14:38:22 -0700

    Added fields for outcomes and deliverables total

--- getDataFromSpreadsheet.js
+++ getDataFromSpreadsheet.js
@@ -15,6 +15,7 @@ async function getDataFromSpreadsheet() {
 
     const rows = await rawSheet.getRows().
       then((rowRes => {
+        // console.log(rowRes)
         for (let j = 0; j < rowRes.length; j += 1) {
           let student = {
             courseTitle: doc.sheetsByIndex[i].title,
@@ -22,6 +23,7 @@ async function getDataFromSpreadsheet() {
             email: rowRes[j].Email,
             studentId: rowRes[j].StudentId,
             absence: rowRes[j].Absence,
+            deliverables: rowRes[j].Deliverables,
             late: rowRes[j].Late,
             Week1: rowRes[j].Week1,
             Week2: rowRes[j].Week2,
@@ -35,23 +37,30 @@ async function getDataFromSpreadsheet() {
             Week10: rowRes[j].Week10,
             Week11: rowRes[j].Week11,
             Week12: rowRes[j].Week12,
-            hw1: rowRes[j]['HW 1'],
-            hw2: rowRes[j]['HW 2'],
-            hw3: rowRes[j]['HW 3'],
-            hw4: rowRes[j]['HW 4'],
-            hw5: rowRes[j]['HW 5'],
-            hw6: rowRes[j]['HW 6'],
-            hw7: rowRes[j]['HW 7'],
-            hw8: rowRes[j]['HW 8'],
-            hw9: rowRes[j]['HW 9'],
-            hw10: rowRes[j]['HW 10'],
-            project1: rowRes[j]['Project 1'],
-            project2: rowRes[j]['Project 2'],
-            project3: rowRes[j]['Project 3'],
+            hw1: rowRes[j]['HW1'],
+            hw2: rowRes[j]['HW2'],
+            hw3: rowRes[j]['HW3'],
+            hw4: rowRes[j]['HW4'],
+            hw5: rowRes[j]['HW5'],
+            hw6: rowRes[j]['HW6'],
+            hw7: rowRes[j]['HW7'],
+            hw8: rowRes[j]['HW8'],
+            hw9: rowRes[j]['HW9'],
+            hw10: rowRes[j]['HW10'],
+            hw11: rowRes[j]['HW11'],
+            hw12: rowRes[j]['HW12'],
+            hw13: rowRes[j]['HW13'],
+            hw14: rowRes[j]['HW14'],
+            hw15: rowRes[j]['HW15'],
+            hw16: rowRes[j]['HW16'],
+            hw17: rowRes[j]['HW17'],
+            Outcomes1: rowRes[j]['Outcomes1'],
+            Outcomes2: rowRes[j]['Outcomes2'],
+            Outcomes3: rowRes[j]['Outcomes3'],
+            Outcomes4: rowRes[j]['Outcomes3'],
+            Outcomes5: rowRes[j]['Outcomes3'],
           };
-
           // console.log(student);
-
           allSheetsData.push(student);
         };
       }));
